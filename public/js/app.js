2849f7aac5f0f1f023150d2ca93f2981de370737
Will Schjang <webmaster.lws@gmail.com>
2015-03-30 18:10:56 -0400

    coded in the task view function and the task view template

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,27 @@ Task.prototype.complete = function(){
   this.isComplete = true;
   this.completedOn = new Date();
 }
+
+function TaskView(model){
+  this.$el = $("<div>"); // if we defined this on the prototype they all share a div
+  this.model = model;
+}
+
+TaskView.prototype = {
+  template: _.template($("#task-template").html()),
+  render: function(){
+    var html = this.template(this.model);
+    this.$el.html(html);
+  },
+  init: function(){
+    this.render();
+    this.$el.on("click", this, this.markComplete);
+    $("#tasks").append(this.$el);
+  },
+  markComplete: function(e){
+    var view = e.data;
+    var task = view.model;
+    task.complete();
+    view.render();
+  }
+}
